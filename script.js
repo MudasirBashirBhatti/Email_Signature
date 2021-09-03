20ed3e418a6654ab2a29bac23b0579d7c9f5edbf
Mudasir Bashir <mudasir.bhatti358@gmail.com>
2021-09-03 07:23:05 -0700

    Email signature project Uploaded

---
+++ script.js
@@ -0,0 +1,15 @@
+// change location color 
+function changePin(){
+document.querySelector('.address img').src ='./utils/changePin.png';
+}
+function defaultPin(){
+    document.querySelector('.address img').src ='./utils/pin.png';
+}
+
+// change mobile colour 
+function changeMobile(){
+document.querySelector('.Contact img').src ='./utils/changeMobile.png';   
+}
+function defaultMobile(){
+    document.querySelector('.Contact img').src ='./utils/vintage-phone-receiver.png';
+}
